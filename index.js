5cd1f760da50aeb136973561d95025f15e2bf5de
Viiuska <87257685+Viiuska@users.noreply.github.com>
2023-03-17 15:45:36 +0200

    Simple member API

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 const express = require('express');
 const path = require('path')
-const members = require('./Members')
-const moment = require('moment')
+
+const logger = require('./middleware/logger')
 
 const app = express();
 
-
-const logger = (req,res, next)=>{
-    console.log(req.protocol+"://"+req.get('host')+req.originalUrl+":"+moment().format());
-    next()
-}
 //Init middleware
-app.use(logger)
+// app.use(logger)
 
-// Get all members
-app.get('/api/members', (req, res)=>{
-    res.json(members)
-})
+
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-
+app.use('/api/members', require('./routes/api/members'))
 
 
 
