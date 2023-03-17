1ee01387b321778e595741459993de31c59695b7
Viiuska <87257685+Viiuska@users.noreply.github.com>
2023-03-17 15:56:37 +0200

    Creating a new member

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const path = require('path')
 
-const logger = require('./middleware/logger')
+const logger = require('./middleware/logger');
+const { use } = require('./routes/api/members');
 
 const app = express();
 
 //Init middleware
 // app.use(logger)
 
-
+//Body oarser middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Member API Routes
 app.use('/api/members', require('./routes/api/members'))
 
 
