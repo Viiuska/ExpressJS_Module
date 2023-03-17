4e6ca23662352d7e6a36708a75737aadbb63e1a9
Viiuska <87257685+Viiuska@users.noreply.github.com>
2023-03-17 20:25:09 +0200

    ExpressJS module done

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const path = require('path')
-
-const logger = require('./middleware/logger');
-const { use } = require('./routes/api/members');
+const expressValidator = require('express-validator')
+//const logger = require('./middleware/logger');
+//const { use } = require('./routes/api/members');
+const members = require('./Members')
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, "views"));
+
+app.use(expressValidator())
+
 
 //Init middleware
 // app.use(logger)
 
-//Body oarser middleware
+//Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+//Homepage router
+app.get('/', (req, res)=>{
+    res.render('index', {
+        title:"Member App",
+        members
+    })
+
+})
+
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
