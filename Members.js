f2c7410962233cc5f1decf170c458700135181ad
Viiuska <87257685+Viiuska@users.noreply.github.com>
2023-03-17 15:29:50 +0200

    Simple middleware done

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members=[
+    {
+        id:1,
+        name: 'Pedro Pascal',
+        email:'pascal@gmail.com',
+        status: 'active'
+    },
+    {
+        id:2,
+        name: 'Bella Ramsey',
+        email:'bella@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id:3,
+        name: 'Tom Holland',
+        email:'tom@gmail.com',
+        status: 'active'
+    },
+];
+
+module.exports= members;
--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const path = require('path')
+const members = require('./Members')
+const moment = require('moment')
 
 const app = express();
 
-/*
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+
+const logger = (req,res, next)=>{
+    console.log(req.protocol+"://"+req.get('host')+req.originalUrl+":"+moment().format());
+    next()
+}
+//Init middleware
+app.use(logger)
+
+// Get all members
+app.get('/api/members', (req, res)=>{
+    res.json(members)
 })
-*/
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
